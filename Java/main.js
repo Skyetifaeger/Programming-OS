66931566875c134ffb923ad0d19da87e224daaa9
Skye <93218668+Skyetifaeger@users.noreply.github.com>
2022-10-16 19:22:36 -0600

    tut #5

--- Java/main.js
+++ Java/main.js
@@ -1,11 +1,13 @@
 var hello = 'hello world';
 var value = 3;
-var value2 = 10;
+var value2 = 0;
+var adds = 3 + value2
 
 console.log(hello);
 //adds 3 to value 2
 console.log(3 + value2);
-
+console.log(value2 == adds, 'if value2 is equal to adds');
+    
 hello = 'goodbye world';
 
 alert(hello);
