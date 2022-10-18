39273fcfe0531cf8eef24f002e1ba2af8a641f2f
Skye <93218668+Skyetifaeger@users.noreply.github.com>
2022-10-18 07:56:10 -0600

    tut #13

--- Java/main.js
+++ Java/main.js
@@ -5,3 +5,9 @@ function getRandomInt(min, max) {
 }
 // Both inclusive
 console.log(getRandomInt(0, 10));
+
+function addNums(num1, num2) {
+    alert(num1 + num2);
+}
+//????Adds first prompt to the front of the second prompt????
+addNums(prompt("1"), prompt("2"));
