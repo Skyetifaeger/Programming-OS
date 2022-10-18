87020292acfc1f6ca4b990228515d6b66c681820
Skye <93218668+Skyetifaeger@users.noreply.github.com>
2022-10-17 19:12:09 -0600

    tut #12

--- Java/main.js
+++ Java/main.js
@@ -1,12 +1,16 @@
-var myList = [1,2,3,4];
+alert('you should guess a number 1-10000000')
 
-// for(var i=0; i<3; i++) {
-//     console.log(myList[i]);
-// }
+var answer = 1.23;
+var guess;
 
-var i = 0;
-
-while ( myList[i] ) {
-    console.log(myList[i]);
-    i ++;
+for (i=0; i<10000000; i++) {
+    guess = prompt('guess, mortal');
+    if (answer == guess) {
+        alert("I'm surprized, good job")
+        break;
+    } if (guess < 2.23 && guess > -0.23){
+        alert('very close');
+    } else {
+        alert('wrong');
+    }
 }
