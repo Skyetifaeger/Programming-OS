3d852ef852b9e9e1259c70cc98e74c7ae532d3e5
Skye <93218668+Skyetifaeger@users.noreply.github.com>
2022-11-01 08:11:17 -0600

    starting react

--- Java/main.js
+++ Java/main.js
@@ -5,9 +5,3 @@ function getRandomInt(min, max) {
 }
 // Both inclusive
 console.log(getRandomInt(0, 10));
-
-function addNums(num1, num2) {
-    return alert(num1 + num2);
-}
-
-addNums(prompt("1"), prompt("2"));
