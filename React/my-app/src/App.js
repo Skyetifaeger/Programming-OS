4649ce222f817b61a37c7686ad963843d96c234a
Skye <93218668+Skyetifaeger@users.noreply.github.com>
2022-11-09 08:34:00 -0700

    understanding components

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -3,46 +3,23 @@ import './App.css';
 import { Component } from 'react';
 
 function App() {
-
-  this.state = {
-    toggle: true
-  }
-
-  this.toggle = () => {
-    this.setState({
-      toggle: !this.state.toggle
-    }
-    )
-  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <Welcome text="welcome to react" />
         </p>
-        {this.state.toggle &&
-          <p>This should show and hide</p>
-        }
-        <button class="button" onClick={this.toggle}>Show / Hide</button>
+        <HelloWorld />
       </header>
     </div>
   );
 }
 
-class Welcome extends Component {
-  render() {
-    const { text } = this.props; //This is a place for Javascript
-    return (
-      <h1
-        className="Welcome message"
-      >
-        {text}
-      </h1>
-    )
-  }
+function HelloWorld() {
+  return(
+    <h1>Hello World</h1> 
+  )
 }
-
 
 export default App;
