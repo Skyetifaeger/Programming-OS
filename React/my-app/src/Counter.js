13a0f7cf103bf71c3177b6b8ea0c340d14f170fd
Skye <93218668+Skyetifaeger@users.noreply.github.com>
2022-11-11 07:59:05 -0700

    making the counter look nice

--- React/my-app/src/Counter.js
+++ React/my-app/src/Counter.js
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 export function Counter() {
     const [count, setCount] = useState(0);
     return (
-        <div>
-            <h3>{count}</h3>
-            <button
-                onClick={() => setCount(count - 1)}
-            >-</button>
-            <button
-                onClick={() => setCount(count + 1)}
-            >+</button>
+        <div class ="App-header">
+            <h3 class="App-header">{count}</h3>
+                <button
+                    onClick={() => setCount(count - 1)}
+                >-</button>
+                <button
+                    onClick={() => setCount(count + 1)}
+                >+</button>
         </div>
     )
 }
