7ee8322dc9ebb43a0ace43a9621d579d81aa5c72
Skye <93218668+Skyetifaeger@users.noreply.github.com>
2022-12-04 15:27:59 -0700

    Update App.js

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,8 +1,8 @@
-import React, { propTypes } from 'react';
+
 import './App.css';
-import { Component } from 'react';
 import { Counter } from './Counter';
 import { Accordion } from './Accordion'
+
 
 function App() {
   return (
