10cc2b5ce4c7207bc14007d86e9da4d1bf6de332
Skye <93218668+Skyetifaeger@users.noreply.github.com>
2022-12-12 08:59:42 -0700

    tut

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import { Counter } from './Counter';
 import { Accordion } from './Accordion'
+import React, { useState } from 'react';
+import { Input } from './input';
 
 
 function App() {
+  const [issudoToggled, setsudoToggled] = useState(1)
+  const [count, setCount] = useState(1);
   return (
-    <div>
-      <Counter />
-      <Accordion />
+    <div class="counterAccordion">
+      <button
+        onClick={() => setsudoToggled(issudoToggled == 2)}
+      >Counter + Accordion</button>
+
+      {issudoToggled == 1 ?
+        <Counter />
+        : <Accordion />
+      }
     </div>
   );
 }
