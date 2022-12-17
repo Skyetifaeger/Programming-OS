38493b500d4a5e99f0e0caa3a7705a1aec43b6e3
Skye <93218668+Skyetifaeger@users.noreply.github.com>
2022-12-17 15:16:49 -0700

    made look nice

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -7,17 +7,17 @@ import { Input } from './input';
 
 function App() {
   const [issudoToggled, setsudoToggled] = useState(1)
-  const [count, setCount] = useState(1);
   console.log(issudoToggled);
   return (
     <div class="pageManager">
       <button
+      style={{ fontSize: '20px'}}
         onClick={() => {
           if (issudoToggled === 3) {
             setsudoToggled(1);
           } else {setsudoToggled(issudoToggled + 1);}
         }}
-      >Counter + Accordion</button>
+      >Content Switcher</button>
 
       {issudoToggled == 1 ?
         <Counter />
