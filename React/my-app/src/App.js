d8b3f957dea8eadf742bf76e3e9ff65660d49c86
Skye <93218668+Skyetifaeger@users.noreply.github.com>
2022-12-18 16:11:24 -0700

    moved pagemanager

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,35 +1,13 @@
 import './App.css';
-import { Counter } from './Counter';
-import { Accordion } from './Accordion'
 import React, { useState } from 'react';
-import { Input } from './input';
-import { MoviesList } from './Movies/moviesList';
+import { PageManager } from './pageManager';
 
 
 function App() {
   const [issudoToggled, setsudoToggled] = useState(1)
   console.log(issudoToggled);
   return (
-    <div class="pageManager">
-      <button
-        style={{ fontSize: '20px' }}
-        onClick={() => {
-          if (issudoToggled === 4) {
-            setsudoToggled(1);
-          } else { setsudoToggled(issudoToggled + 1); }
-        }}
-      >Content Switcher</button>
-      <div class = "render">
-        {issudoToggled === 1 ?
-          <Counter />
-          : issudoToggled === 2 ?
-            <Accordion />
-            : issudoToggled === 3 ?
-              <Input />
-              : <MoviesList />
-        }
-      </div>
-    </div>
+    <PageManager issudoToggled = {issudoToggled} setsudoToggled ={setsudoToggled}/>
   );
 }
 
---
+++ React/my-app/src/pageManager.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './App.css';
+import { Counter } from './Counter';
+import { Accordion } from './Accordion';
+import { Input } from './input';
+import { MoviesList } from './Movies/moviesList';
+
+export function PageManager({issudoToggled, setsudoToggled}) {
+    return (
+        <div class = "pageManager">
+            <button
+                style={{ fontSize: '20px' }}
+                onClick={() => {
+                    if (issudoToggled === 4) {
+                        setsudoToggled(1);
+                    } else { setsudoToggled(issudoToggled + 1); }
+                }}
+            >Content Switcher</button>
+            <div class="render">
+                {issudoToggled === 1 ?
+                    <Counter />
+                    : issudoToggled === 2 ?
+                        <Accordion />
+                        : issudoToggled === 3 ?
+                            <Input />
+                            : <MoviesList />
+                }
+            </div>
+        </div>
+    )
+}
