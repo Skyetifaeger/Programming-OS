cf197d8250e2af74063ea159d6e855db6f32c7fe
Skye <93218668+Skyetifaeger@users.noreply.github.com>
2023-01-18 18:50:05 -0700

    imagesAndCss

--- React/my-app/src/Movies/moviesList.js
+++ React/my-app/src/Movies/moviesList.js
@@ -4,9 +4,14 @@ import { Filter } from "../filter";
 
 const API_URL = 
 'https://api.themoviedb.org/3/discover/movie?api_key=c305db71fd7bdaaa011546a949f5404f&language=us_EN&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_original_language=ja'
+const CONFIG_URL =
+'https://api.themoviedb.org/3/configuration?api_key=c305db71fd7bdaaa011546a949f5404f' 
+
 export function MoviesList() {
     const [filter, setFilter] = useState("")
     const [movies, setMovies] = useState([])
+    const [config, setConfig] = useState({})
+    
     const getMovies = async () => {
         try{
             const res = await fetch(API_URL)
@@ -17,18 +22,29 @@ export function MoviesList() {
         }
     }
 
+    const getConfig = async () => {
+        try{
+            const res = await fetch(CONFIG_URL)
+            const config = await res.json()
+            setConfig(config)
+        } catch (e){
+            console.error(e)
+        }
+    }
+
     useEffect(() => {
         getMovies()
+        getConfig()
     })
     
     return (
         <div class="movies">
             <Filter filter={filter} setFilter={setFilter}/>
-            <ul>
+            <ul className = "movies-list">
                 {movies.filter((movie) =>
                     movie.title.toLowerCase().includes(filter.toLowerCase())
                 ).map((movie) => (
-                    <Movie key={movie.id} movie={movie}/>
+                    <Movie key={movie.id} config = {config} movie={movie}/>
                 )
                 )}
             </ul>
