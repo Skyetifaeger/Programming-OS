c6fb8c460d0815c14fc6b633b983db7e969b61ff
Skye <93218668+Skyetifaeger@users.noreply.github.com>
2023-03-05 15:28:40 -0700

    added final project

---
+++ finalProject/finalproject/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='Header'>
+        <h1>To Do</h1>
+      </header>
+      <div>
+        <button className='addTaskButton'>Add Task</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
